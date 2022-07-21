88926eb530a0bb3c9277e936d23463ff5bcc0cd5
shaokai-hou <17392652059@163.com>
2022-07-21 16:04:17 +0800

    demo init

---
+++ demo-springboot/demo-rabbitmq/src/main/java/com/haohao/rabbitmq/RabbitmqApplication.java
@@ -0,0 +1,15 @@
+package com.haohao.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author haohao
+ * @date 2022年07月21日 15:52
+ */
+@SpringBootApplication
+public class RabbitmqApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqApplication.class, args);
+    }
+}
